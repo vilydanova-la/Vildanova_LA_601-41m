ed7d32aeb3f5c84a7fbdc84bbdc4be7acf159ffb
Liliya <vilydanova_la@edu.surgu.ru>
2025-10-17 16:43:46 +0500

    Create code.py

---
+++ IT-project/lab_3/program/code.py
@@ -0,0 +1,14 @@
+def median(data: list) -> float:
+    if not data:
+        return None #если список пустой
+    sorted_values = sorted(data)
+    n = len(sorted_values)
+    mid = n // 2
+
+    if n % 2 == 1:  # нечетное количество элементов
+        return float(sorted_values[mid])
+    else:  # четное количество элементов
+        return (sorted_values[mid - 1] + sorted_values[mid]) / 2
+
+nums = []
+print(median(nums))  
